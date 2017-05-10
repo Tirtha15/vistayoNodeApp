d7809244bb433b9dcf63cf1c6cd08b01726657c0
tirtha-slicepay <tirtha.ghosh@slicepay.in>
2017-05-10 18:00:34 +0530

    theme page, destination page, filter destination from theme page

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,4 +1,5 @@
 var _ = require('underscore');
+var async = require('async');
 var db = require('./../config/db');
 var config = require('./../config');
 var mongoDb = db.mongo;
@@ -49,31 +50,127 @@ var searchController  = {
 
     themeSearch: function(req, res){
         var keyword = req.params.keyword;
-        var dataToSend = [{
-            keyword: keyword,
-            name: 'honeymoon',
-            imgUrls: ['img1', 'img2'],
-            attractions: ['wilflife', 'adventure'],
-            monthOftravel: ['january', 'february', 'march'],
-            idealFor: ['adventurers']
-        }];
 
-        res.ok(dataToSend);
+        var idealForSelections = req.query.idealFor ? req.query.idealFor.split(',') : null;
+        var monthOfTravelSelections = req.query.monthOfTravel ? req.query.monthOfTravel.split(',') : null;
+        var attractionsSelections = req.query.attractions ? req.query.attractions.split(',') : null;
+        var isInternational = req.query.isInternational || false;
+        var isDomestic = req.query.isDomestic || false;
+
+
+
+        async.auto({
+            travelTheme: function(cb){
+                var travelThemes = mongoDb.get('travelThemes');
+
+                travelThemes.findOne({
+                    slug: keyword
+                },{}, function(err, theme){
+                    if(err)
+                      return cb(err);
+                    if(!theme)
+                      return cb({
+                          code: 'NOT_FOUND',
+                          msg: 'requested travel theme not found'
+                      });
+
+                    return cb(null, theme);
+                });
+            },
+            destinations:['travelTheme', function(results, cb){
+                var destinationsCol = mongoDb.get('destinations');
+
+                var findCriteria = {};
+
+                findCriteria.travelThemes = results.travelTheme.uuid;
+
+                if(isInternational)
+                    findCriteria.isInternational = true;
+
+                if(isDomestic)
+                    findCriteria.isInternational = false;
+
+                if(isInternational && isDomestic)
+                    delete findCriteria.isInternational;
+
+                if(idealForSelections)
+                    findCriteria.idealFor = { $in: idealForSelections};
+
+                if(monthOfTravelSelections)
+                    findCriteria.monthOfTravel = { $in: monthOfTravelSelections};
+
+                if(attractionsSelections)
+                    findCriteria.attractions = { $in: attractionsSelections};
+
+                destinationsCol.find(findCriteria, {}, function(err, matchingDestination){
+                    if(err)
+                      return cb(err);
+                    return cb(null, matchingDestination);
+                });
+            }],
+        }, function(err, results){
+            if(err){
+                if(err){
+                    if(err.code && err.code === 'NOT_FOUND'){
+                        return res.notFound(err);
+                    }
+                    if(err.code && err.code === 'BAD_REQUEST'){
+                        return res.badRequest(err);
+                    }
+                    return res.serverError(err);
+                }
+            }
+
+            return res.ok(results);
+        });
     },
 
     destinationSearch: function(req, res){
         var keyword = req.params.keyword;
-        var dataToSend = [{
-            keyword: keyword,
-            name: 'honeymoon',
-            cities:['bangalore'],
-            imgUrls: ['img1', 'img2'],
-            attractions: ['wilflife', 'adventure'],
-            monthOftravel: ['january', 'february', 'march'],
-            idealFor: ['adventurers']
-        }];
 
-        res.ok(dataToSend);
+        async.auto({
+            destination: function(cb){
+                var destinationsCol = mongoDb.get('destinations');
+
+                destinationsCol.findOne({
+                    slug: keyword
+                }, {}, function(err, destination){
+                    if(err)
+                      return cb(err);
+                    if(!destination)
+                      return cb({
+                          code: 'NOT_FOUND',
+                          msg: 'requested destination not found'
+                      });
+                    return cb(null, destination);
+                });
+            },
+            packages:['destination', function(results, cb){
+                var packagesCol = mongoDb.get('packages');
+
+                packagesCol.find({
+                    destination: results.destination.uuid
+                }, {}, function(err, packages){
+                    if(err)
+                      return cb(err);
+                    return cb(null, packages);
+                });
+            }]
+        }, function(err, results){
+            if(err){
+                if(err){
+                    if(err.code && err.code === 'NOT_FOUND'){
+                        return res.notFound(err);
+                    }
+                    if(err.code && err.code === 'BAD_REQUEST'){
+                        return res.badRequest(err);
+                    }
+                    return res.serverError(err);
+                }
+            }
+
+            return res.ok(results);
+        });
     }
 
 };
